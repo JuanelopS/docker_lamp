bcc6c7ef4e000322e2d79ad044602837386db304
Juan Gavira <contacto@juangavira.me>
2023-05-05 22:31:55 +0200

    little css

--- www/layout/footer.php
+++ www/layout/footer.php
@@ -1,8 +1,9 @@
 </main>
 <footer class="footer">
     <div class="footer-content">
-        <p>Juan Gavira <?= date("Y") ?></p>
+        <p>Juan Gavira <?= date("Y") ?>&copy;</p>
     </div>
 </footer>
 </body>
+
 </html>
