7794df6d87e70e00670f13dac99ad923fa2919c0
Juan Gavira <contacto@juangavira.me>
2023-05-06 15:26:50 +0200

    added some todos and comments

--- www/php/database/user_register.php
+++ www/php/database/user_register.php
@@ -16,10 +16,22 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$database", $user_db, $pass_db);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // insert query (rol:2 -> normal user)
     $sql = "INSERT INTO users (email, password, name, surname, rol) VALUES ('$email', '$password', '$name', '$surname', 2)";
     // use exec() because no results are returned
     $conn->exec($sql);
+
+    /* FIXME:
+        - DUPLICATED REGISTRATION EMAILS
+        - CHANGE TO PREPARED STATMENTS AND BOUND PARAMETERS -> https://www.w3schools.com/php/php_mysql_prepared_statements.asp
+        - PROTECT PASSWORDS
+     */
+
+    // load view
+    include($_SERVER['DOCUMENT_ROOT'] . '/layout/header.php');
     echo "User registered successfully!";
+    include($_SERVER['DOCUMENT_ROOT'] . '/layout/footer.php');
+
 } catch (PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
 }
