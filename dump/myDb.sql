2603705eb6270ee2ba4283008d74a0f99196a780
Juan Gavira <contacto@juangavira.me>
2023-05-07 12:54:29 +0200

    added some tables and relations at db (items,sales,order_details,etc...)

--- dump/myDb.sql
+++ dump/myDb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db
--- Tiempo de generación: 07-05-2023 a las 09:27:30
+-- Tiempo de generación: 07-05-2023 a las 10:53:32
 -- Versión del servidor: 8.0.33
--- Versión de PHP: 8.0.19
+-- Versión de PHP: 8.1.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,12 +24,54 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `items`
+--
+
+CREATE TABLE `items` (
+  `id` int NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `stock` int NOT NULL,
+  `prize` float NOT NULL,
+  `type` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `paid` float NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `orders_details`
+--
+
+CREATE TABLE `orders_details` (
+  `item_id` int NOT NULL,
+  `item_name` varchar(100) COLLATE utf16_unicode_ci NOT NULL,
+  `item_prize` int NOT NULL,
+  `item_amount` int NOT NULL,
+  `item_paid` float NOT NULL,
+  `order_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `roles`
 --
 
 CREATE TABLE `roles` (
   `id_rol` tinyint NOT NULL,
-  `name` varchar(15) COLLATE utf16_unicode_ci NOT NULL
+  `name` varchar(15) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
 
 --
@@ -49,7 +91,7 @@ INSERT INTO `roles` (`id_rol`, `name`) VALUES
 CREATE TABLE `users` (
   `id` int NOT NULL,
   `email` varchar(100) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
-  `password` varchar(100) COLLATE utf16_unicode_ci NOT NULL,
+  `password` varchar(100) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
   `name` varchar(40) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
   `surname` varchar(100) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
   `register_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -63,11 +105,32 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `email`, `password`, `name`, `surname`, `register_date`, `rol`) VALUES
 (1, 'test@test.com', 'kasjdhfkajsdhfjdsafh', 'name', 'surname', '2023-05-06 12:58:23', 2),
 (4, 'test2@test.com', 'asdlfjasdlkfj', 'alksfdhaf', '1345345', '2023-05-06 13:19:58', 2),
-(5, 'email@test.com', 'alfjasdjkf', ' 3450', '34u95u07', '2023-05-06 13:28:22', 2);
+(5, 'email@test.com', 'alfjasdjkf', ' 3450', '34u95u07', '2023-05-06 13:28:22', 2),
+(6, 'juan@juan.com', 'juan', 'juan', 'juan', '2023-05-07 09:48:43', 2);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indices de la tabla `orders_details`
+--
+ALTER TABLE `orders_details`
+  ADD KEY `order_id_for_details` (`order_id`),
+  ADD KEY `order_id` (`order_id`);
 
 --
 -- Indices de la tabla `roles`
@@ -88,14 +151,38 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Filtros para la tabla `orders_details`
+--
+ALTER TABLE `orders_details`
+  ADD CONSTRAINT `orders_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Filtros para la tabla `users`
