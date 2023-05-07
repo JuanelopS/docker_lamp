8f7721ec573a91cc0f4a27ec4bae6d1c7c7a5213
Juan Gavira <contacto@juangavira.me>
2023-05-07 18:21:10 +0200

    small changes to db tables and relations

--- dump/myDb.sql
+++ dump/myDb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db
--- Tiempo de generación: 07-05-2023 a las 16:06:27
+-- Tiempo de generación: 07-05-2023 a las 16:20:27
 -- Versión del servidor: 8.0.33
 -- Versión de PHP: 8.0.19
 
